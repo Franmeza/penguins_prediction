3f0a148dc56c7a1dd4fb9f859995b29ed6f329a5
Francisco Meza Rios <ing.meza.francisco@gmail.com>
2025-07-30 08:32:54 -0600

    Add st.info and st.expander elements

--- app.py
+++ app.py
@@ -4,3 +4,7 @@ import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 
 st.title('Penguine Specie Prediction ML app')
+st.info("This is end-to-end Machine Learning app")
+
+with st.expander("Data"):
+  pass
