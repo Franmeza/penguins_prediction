1cebf1fd553f0d79aaea9344a5a21db9305519de
Francisco Meza Rios <ing.meza.francisco@gmail.com>
2025-07-30 08:46:52 -0600

    Add elements to display content and structure of the app

--- app.py
+++ app.py
@@ -8,3 +8,15 @@ st.info("This is end-to-end Machine Learning app")
 
 with st.expander("Data"):
   pass
+
+with st.expander("Data Visualization"):
+  pass
+  
+with st.expander("Input Data"):
+  pass
+
+with st.expander("Data Preparation"):
+  pass
+
+with st.sidebar:
+  pass
